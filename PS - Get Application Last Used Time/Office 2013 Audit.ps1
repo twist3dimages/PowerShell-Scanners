a1d7b11f813356d221c3b85854e64c28625baf86
Charley <33134502+twist3dimages@users.noreply.github.com>
2021-05-25 11:18:39 -0400

    Update Office 2013 Audit.ps1

--- PS - Get Application Last Used Time/Office 2013 Audit.ps1
+++ PS - Get Application Last Used Time/Office 2013 Audit.ps1
@@ -8,5 +8,5 @@ Get-WMIObject -Namespace $namespace -Class $classname -ComputerName $computer -E
     Where-Object { ($_.ProductName -eq 'Microsoft Office Professional Plus 2013') -and ( $_.Accesses -ge 1 -and $_.LastUse -gt "20160101" -and -not( [int]$_.LastUse.Substring(0,8)-eq "19800000") ) } |
       
     Select-Object Caption, ProductName, Name, @{n='LastUse';e={([System.Management.ManagementDateTimeConverter]::ToDateTime($_.LastUse)).ToString("MM/dd/yyyy")}} #| Format-Table -autosize
-    #{[int]($_.Lastuse.Substring(0, 8))}},PSComputername, InstallDate
+    #{[int]($_.Lastuse.Substring(0, 8))}},PSComputername, InstallDate, Platform
     
